aac27306cf57501c4d8a0f83b5ddecbbfa3fa9a7
Esina Svetlana <montesum1@gmail.com>
2025-12-12 10:06:07 +0300

    закомменчен Configuration.holdBrowserOpen

--- src/test/java/pages/TestBase.java
+++ src/test/java/pages/TestBase.java
@@ -9,7 +9,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+//        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000;
     }
 
